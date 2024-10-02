1ff0e99bb131b9a4c2bbc352320ef8759d5310e6
Matheus Correa <matheus.barrosc@gmail.com>
2024-10-02 19:21:42 -0300

    Ajuste para tratar uma lista de partidas e não somente uma.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import lol_infos.lol_apis as la
 import lol_infos.lol_data_cleaning as ldc
+import mongo_code.db_connection as dbc
 import json
 
 game_name = input("Digite seu nick (sem a tag): ")
@@ -11,6 +12,14 @@ lol_acc_puuid = lol_acc.get_puuid()
 
 all_matchs = lol_acc.get_all_matchs(lol_acc_puuid)
 
-match_infos = lol_acc.get_match_geral_info(all_matchs[3])
+match_infos = []
 
-match_data = ldc.organize_match_data(match_infos)
+for i in all_matchs:
+    match_infos.append(lol_acc.get_match_geral_info(i))
+
+match_data_cleaned = []
+
+for i in match_infos:
+    match_data_cleaned.append(ldc.organize_match_data(i))
+
+post_match = dbc.create_match_db(match_data_cleaned)
