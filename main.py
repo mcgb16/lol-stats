6db945fb73c043f835e761c7540bf502d5e3d3e2
Matheus Correa <matheus.barrosc@gmail.com>
2024-10-18 16:07:00 -0300

    Organização do código.

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import lol_infos.lol_apis as la
 import lol_infos.lol_data_cleaning as ldc
-import mongo_code.db_connection as dbc
+import mongo_code.db_connection as db_conn
 import json
 
-game_name = input("Digite seu nick (sem a tag): ")
-game_tag = input("Digite sua tag: ")
+def ask_name_tag():
+    player_name = input("Digite seu nick (sem a tag): ")
+    player_tag = input("Digite sua tag: ")
 
-lol_acc = la.LolVerifier(game_name, game_tag)
+    return player_name, player_tag
 
-lol_acc_puuid = lol_acc.get_puuid()
+def connect_with_riot_api(name, tag):
+    lol_acc = la.LolVerifier(name, tag)
+    lol_acc_puuid = lol_acc.get_puuid()
 
-all_matchs = lol_acc.get_all_matchs(lol_acc_puuid)
+    return lol_acc, lol_acc_puuid
 
-match_infos = []
+def check_matchs(lol_acc, puuid):
+    all_matchs = lol_acc.get_all_matchs(puuid)
 
-for i in all_matchs:
-    match_infos.append(lol_acc.get_match_geral_info(i))
+    print(len(all_matchs))
 
-match_data_cleaned = []
+    match_infos = []
 
-for i in match_infos:
-    match_data_cleaned.append(ldc.organize_match_data(i))
+    for i in all_matchs:
+        match_exists_db = db_conn.find_match(i)
+        if match_exists_db == None:
+            match_infos.append(lol_acc.get_match_geral_info(i))
+        else:
+            continue
 
-post_match = dbc.create_match_db(match_data_cleaned)
+    match_data_cleaned = []
+
+    for i in match_infos:
+        match_data_cleaned.append(ldc.organize_match_geral_data(i))
+
+    try:
+        save_match_db = db_conn.create_match_db(match_data_cleaned)
+    except Exception as e:
+        save_match_db = f"Erro: {e}"
+
+    return save_match_db
+
+if __name__ == "__main__":
+    pl_name, pl_tag = ask_name_tag()
+
+    acc, acc_puuid = connect_with_riot_api(pl_name, pl_tag)
+
+    match_db_result = check_matchs(acc, acc_puuid)
