9ca62c7d4f32db3aeeb9d5116e5a7497237b37d7
Matheus Correa <matheus.barrosc@gmail.com>
2024-10-28 17:45:59 -0300

    Ajuste mínimo no código de nomenclatura e exclusão do import da biblioteca de json.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import lol_infos.lol_apis as la
 import lol_infos.lol_data_cleaning as ldc
 import mongo_code.db_connection as db_conn
-import json
 
 def ask_name_tag():
     player_name = input("Digite seu nick (sem a tag): ")
@@ -15,7 +14,7 @@ def connect_with_riot_api(name, tag):
 
     return lol_acc, lol_acc_puuid
 
-def check_matchs(lol_acc, puuid):
+def etl_matchs(lol_acc, puuid):
     all_matchs = lol_acc.get_all_matchs(puuid)
 
     print(len(all_matchs))
@@ -29,10 +28,15 @@ def check_matchs(lol_acc, puuid):
         else:
             continue
 
-    match_data_cleaned = []
+    match_data_organized = []
 
     for i in match_infos:
-        match_data_cleaned.append(ldc.organize_match_geral_data(i))
+        match_data_organized.append(ldc.organize_match_geral_data(i))
+
+    match_data_cleaned = []
+
+    for i in match_data_organized:
+        match_data_cleaned.append(ldc.transforming_match_data(i))
 
     try:
         save_match_db = db_conn.create_match_db(match_data_cleaned)
@@ -46,4 +50,4 @@ if __name__ == "__main__":
 
     acc, acc_puuid = connect_with_riot_api(pl_name, pl_tag)
 
-    match_db_result = check_matchs(acc, acc_puuid)
+    match_db_result = etl_matchs(acc, acc_puuid)
