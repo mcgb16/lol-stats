9259c9339ef04351ac4ad9a4fe96d582735f0a04
Matheus Correa <matheus.barrosc@gmail.com>
2024-11-11 16:38:28 -0300

    Ajustes para conseguir analisar o winrate e pickrate do usuário com os campeões.

--- lol_infos/lol_data_analysis.py
+++ lol_infos/lol_data_analysis.py
@@ -92,6 +92,9 @@ class AnalysePlayer:
         ft_participation_mean = ft_kill_mean + ft_assist_mean
         team_gold_percentage_mean = np.mean(player_df['percentageTeamGold'])
         team_dpm_percentage_mean = np.mean(player_df['percentageTeamDpmChampions'])
+        winrate_mean = np.mean(player_df["win"])
+        wins = np.sum(player_df["win"])
+        loses = np.sum(player_df["win"] == False)
 
         # Extremos
         max_kda = np.max(player_df['kda'])
@@ -131,7 +134,9 @@ class AnalysePlayer:
             "max_percentage_dpm_champions": max_percentage_dpm_champions,
             "min_percentage_dpm_champions": min_percentage_dpm_champions,
             "max_gold_efficiency": max_gold_efficiency,
-            "min_gold_efficiency": min_gold_efficiency
+            "min_gold_efficiency": min_gold_efficiency,
+            "wins": wins,
+            "loses": loses
         }
 
         mean_dict = {
@@ -150,7 +155,10 @@ class AnalysePlayer:
             "ft_assist": ft_assist_mean,
             "ft_participation": ft_participation_mean,
             "team_gold_percentage": team_gold_percentage_mean,
-            "team_dpm_percentage": team_dpm_percentage_mean
+            "team_dpm_percentage": team_dpm_percentage_mean,
+            "winrate": winrate_mean,
+            "wins": wins,
+            "loses": loses
         }
 
         max_min_df = pd.DataFrame([max_min_dict])
@@ -176,6 +184,10 @@ class AnalysePlayer:
         ft_participation_mean = ft_kill_mean + ft_assist_mean
         team_gold_percentage_mean = player_df['percentageTeamGold'].mean()
         team_dpm_percentage_mean = player_df['percentageTeamDpmChampions'].mean()
+        winrate_mean = player_df['win'].mean()
+        wins = player_df['win'].apply(lambda x: x[x == True].count())
+        loses = player_df['win'].apply(lambda x: x[x == False].count())
+        pickrate = player_df.size()
 
         # Extremos
         max_kda = player_df['kda'].max()
@@ -215,7 +227,10 @@ class AnalysePlayer:
             "max_percentage_dpm_champions": max_percentage_dpm_champions,
             "min_percentage_dpm_champions": min_percentage_dpm_champions,
             "max_gold_efficiency": max_gold_efficiency,
-            "min_gold_efficiency": min_gold_efficiency
+            "min_gold_efficiency": min_gold_efficiency,
+            "pickrate": pickrate,
+            "wins": wins,
+            "loses": loses
         }
 
         mean_dict = {
@@ -234,7 +249,11 @@ class AnalysePlayer:
             "ft_assist": ft_assist_mean,
             "ft_participation": ft_participation_mean,
             "team_gold_percentage": team_gold_percentage_mean,
-            "team_dpm_percentage": team_dpm_percentage_mean
+            "team_dpm_percentage": team_dpm_percentage_mean,
+            "winrate": winrate_mean,
+            "pickrate": pickrate,
+            "wins": wins,
+            "loses": loses
         }
 
         max_min_df = pd.DataFrame(max_min_dict)
@@ -243,13 +262,15 @@ class AnalysePlayer:
         return mean_df, max_min_df
 
     def __create_mean_plots(self, mean_df):
-        # plots: fb/ft ; %g/%d/%kp ; dpmc/dpmt/ge/gs/geff ; vs/kda/fpm
-        fig, axes = plt.subplots(2, 2, figsize=(10, 6))
+        # plots: fb/ft ; %g/%d/%kp ; dpmc/dpmt/gpm/geff ; vspm/kda/fpm ; picks ; wr
+        fig, axes = plt.subplots(2, 3, figsize=(10, 6))
 
-        fb_plot = axes [0,0]
-        team_comparison_plot = axes [0,1]
-        dmg_gold_plot = axes [1,0]
-        basic_plot = axes [1,1]
+        fb_plot = axes[0,0]
+        team_comparison_plot = axes[0,1]
+        winrate_plot = axes[0,2]
+        dmg_gold_plot = axes[1,0]
+        basic_plot = axes[1,1]
+        pickrate_plot = axes[1,2]
 
         bar_width = 0.35
         indices = range(len(mean_df))
@@ -269,12 +290,19 @@ class AnalysePlayer:
             dmg_gold_plot.bar(indices[i] - bar_width / 2, mean_df.loc[champion, "dpm_champion"], width=bar_width, label='DPM Champion' if i == 0 else "", color='skyblue')
             dmg_gold_plot.bar(indices[i] + bar_width / 2, mean_df.loc[champion, "dpm_turret"], width=bar_width, label='DPM Turret' if i == 0 else "", color='dodgerblue')
 
+            winrate_plot.bar(indices[i] - bar_width / 2, mean_df.loc[champion, "wins"], width=bar_width, label='Wins' if i == 0 else "", color='green')
+            winrate_plot.bar(indices[i] + bar_width / 2, mean_df.loc[champion, "loses"], width=bar_width, label='Loses' if i == 0 else "", color='red')
+
             basic_plot.bar(indices[i] + bar_width / 2, mean_df.loc[champion, "kda"], width=bar_width, label='KDA' if i == 0 else "", color='dodgerblue')
 
         ax3 = basic_plot.twinx()
         ax3.plot(indices,mean_df["fpm"], color='red', marker='o', linestyle='-', label='Farm per Minute', linewidth=2)
         ax3.plot(indices,mean_df["vspm"], color='purple', marker='^', linestyle='--', label='Vision Score per Minute', linewidth=2)
         ax3.set_ylabel('Farm/Vision Score per Minute')
+
+        winrate_plot3 = basic_plot.twinx()
+        winrate_plot3.plot(indices,mean_df["winrate"], color='blue', marker='o', linestyle='-', label='Winrate', linewidth=2)
+        winrate_plot3.set_ylabel('% Winrate')
 
         lines, labels = basic_plot.get_legend_handles_labels()
         lines2, labels2 = ax3.get_legend_handles_labels()
@@ -303,6 +331,12 @@ class AnalysePlayer:
         dmg_gold_plot.set_ylabel('Quantidade')
         dmg_gold_plot.set_title('Relação de Dano e Gold por Campeão')
 
+        winrate_plot.set_xticks(indices)
+        winrate_plot.set_xticklabels(mean_df.index)
+        winrate_plot.set_xlabel('Campeões')
+        winrate_plot.set_ylabel('Quantidade Jogos')
+        winrate_plot.set_title('Winrate')
+
         basic_plot.set_xticks(indices)
         basic_plot.set_xticklabels(mean_df.index)
         basic_plot.set_xlabel('Campeões')
@@ -325,6 +359,24 @@ class AnalysePlayer:
         team_comparison_plot.set_ylabel('Gold | DPM | KP')
         team_comparison_plot.legend(loc='upper right')
 
+        # Ordenando por winrate para o Pareto
+        sorted_pickrate = mean_df['pickrate'].sort_values(ascending=False)
+
+        # Plotando as barras de winrate
+        sorted_pickrate.plot(kind='bar', color='skyblue', ax=pickrate_plot)
+        pickrate_plot.set_ylabel('Winrate (%)')
+        pickrate_plot.set_xlabel('Campeões')
+        pickrate_plot.set_title('Pareto de Winrate por Campeão')
+
+        # Calculando a linha de porcentagem acumulada
+        cumulative_sum = sorted_pickrate.cumsum()
+        cumulative_percentage = cumulative_sum / cumulative_sum.iloc[-1] * 100
+
+        # Plotando a linha cumulativa no eixo secundário
+        pickrate_plot2 = pickrate_plot.twinx()
+        pickrate_plot2.plot(cumulative_percentage, color='red', marker='D', linestyle='-', label='Cumulativo (%)')
+        pickrate_plot2.set_ylabel('Porcentagem Cumulativa (%)')
+        pickrate_plot2.legend(loc='upper right')
 
         plt.tight_layout()
         plt.show()
