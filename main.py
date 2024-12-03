06c92ea6bcaa84b5cafdbdb95009467fa3dfed64
Matheus Correa <matheus.barrosc@gmail.com>
2024-12-03 15:00:39 -0300

    Ajuste de código para a limpeza das informações dos elos.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import lol_infos.lol_data_cleaning as ldc
 import lol_infos.lol_apis as la
 import basic_code.basic as basic
 import lol_infos.lol_data_analysis as lda
@@ -9,32 +10,13 @@ if __name__ == "__main__":
     lol_acc_puuid = lol_acc.get_puuid()
     lol_acc_infos = lol_acc.get_acc_info(lol_acc_puuid)
     lol_acc_sum_id = lol_acc_infos["id"]
-    pl_elos = lol_acc.get_acc_ranks(lol_acc_sum_id)
-
-    pl_elos_cleaned = []
-    pl_elo_index = {
-        "Queue" : "",
-        "Tier" : "",
-        "Rank" : "",
-        "Lp" : "",
-        "Wins" : "",
-        "Losses" : ""
-    }
-
-    for i in pl_elos:
-        pl_elo_index["Queue"] = i["queueType"]
-        pl_elo_index["Tier"] = i["tier"]
-        pl_elo_index["Rank"] = i["rank"]
-        pl_elo_index["Lp"] = i["leaguePoints"]
-        pl_elo_index["Wins"] = i["wins"]
-        pl_elo_index["Losses"] = i["losses"]
-        pl_elos_cleaned.append(pl_elo_index.copy())
+    pl_elos = ldc.clean_elo_data(lol_acc, lol_acc_sum_id)
 
     pl_history_save = basic.save_player_history(lol_acc, lol_acc_puuid)
 
     player_analysis = lda.AnalysePlayer(lol_acc_puuid)
     dfs_dict, history_games = player_analysis.create_player_analysis()
-    basic_info_list = [pl_name,pl_tag, pl_elos_cleaned]
+    basic_info_list = [pl_name,pl_tag, pl_elos]
 
     player_analysis.create_acc_pie_plot(dfs_dict["no_filter_mean"])
     player_analysis.create_acc_radar_plot(dfs_dict["no_filter_mean"])
