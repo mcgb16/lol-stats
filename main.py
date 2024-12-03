3b19736f5ab38b73ac25ed80286d5d81f01a1a51
Matheus Correa <matheus.barrosc@gmail.com>
2024-12-03 15:55:36 -0300

    Ajuste no código após criação de função para retornos da API.

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import lol_infos.lol_data_analysis as lda
 
 if __name__ == "__main__":    
     pl_name, pl_tag = basic.ask_name_tag()
-    
-    lol_acc = la.LolVerifier(pl_name, pl_tag)
-    lol_acc_puuid = lol_acc.get_puuid()
-    lol_acc_infos = lol_acc.get_acc_info(lol_acc_puuid)
-    lol_acc_sum_id = lol_acc_infos["id"]
-    pl_elos = ldc.clean_elo_data(lol_acc, lol_acc_sum_id)
 
-    pl_history_save = basic.save_player_history(lol_acc, lol_acc_puuid)
+    api_basic = basic.apis_basic_info(pl_name, pl_tag)
 
-    player_analysis = lda.AnalysePlayer(lol_acc_puuid)
+    pl_elos = ldc.clean_elo_data(api_basic["lol_acc"], api_basic["sum_id"])
+
+    pl_history_save = basic.save_player_history(api_basic["lol_acc"], api_basic["puuid"])
+
+    player_analysis = lda.AnalysePlayer(api_basic["puuid"])
     dfs_dict, history_games = player_analysis.create_player_analysis()
     basic_info_list = [pl_name,pl_tag, pl_elos]
 
